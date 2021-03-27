20ed9ac9afd059320cbbadae0a0c0e1a25917e81
nicoarkano <nicoarkano@gmail.com>
2021-03-27 19:39:33 -0300

    Agrego changes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
 
-reactDOM.render(<HelloWorld/>, document.getElementById(app));
+ReactDOM.render(<HelloWorld/>, document.getElementById("app"));
